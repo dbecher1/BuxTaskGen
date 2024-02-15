49ca49e96ce84a6fe82868550274f5546ed997cb
dbecher <debecher93@gmail.com>
2024-02-14 19:09:59 -0500

    Pre-buffer switch update

---
+++ simple_date_helper.py
@@ -0,0 +1,41 @@
+# Python's build in datetime library's dates are immutable
+# Plus I'd rather just write a small util class than import -more- external stuff
+
+WEEKDAY_ENUM = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+class SimpleDate:
+    # Given a month/day that is always given to be the first monday of a week, constructs a nifty little helper
+    def __init__(self, month=1, day=1, year=2024) -> None:
+        self.year = year
+        self.month = month
+        self.day = day
+        self.offset = 0
+
+    # Returns a string representation of the weekday/month/date (e.g. Monday, 2/12)
+    # It then increments its offset to be used for the next call
+    def get_date(self) -> str:
+        out = WEEKDAY_ENUM[self.offset % 7] + ', '
+        out += str(self.month) + '/' + str(self.day)
+        self.increment_date()
+        return out
+    
+    # returns 0-6 for Monday-Sunday
+    def get_offset(self) -> int:
+        return self.offset % 7
+
+    def increment_date(self) -> None:
+        self.offset += 1
+        # Test the
+        if self.month == 2:
+            num_days = 28
+            if self.year % 4 == 0:
+                num_days += 1
+        elif (self.month) == 4 or (self.month) == 6 or (self.month) == 9 or (self.month == 11):
+            num_days = 30
+        else:
+            num_days = 31
+        # Wraparound
+        self.day = (self.day + 1) % (num_days + 1)
+        if self.day == 0:
+            self.day += 1
+            self.month = (self.month + 1) % 13
